6f7d99f58f9767b4955019c8a929c17698aa62d8
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2025-02-05 05:15:51 +0100

    react router dom

--- src/pages/GithubUsers.jsx
+++ src/pages/GithubUsers.jsx
@@ -1,5 +1,18 @@
+import { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users";
 const GithubUsers = () => {
-  return <div>GithubUsers</div>;
+  const [users, setUsers] = useState([]);
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("hello");
+  }, []);
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>incr</button>
+    </div>
+  );
 };
 
 export default GithubUsers;
